9ea465bf40353704213936a28321a7b8d5787be3
Shidhin <shidhincr@gmail.com>
2020-03-17 19:23:52 +0530

    Updates

---
+++ js/load-disqus-comments.js
@@ -0,0 +1,28 @@
+(function() {
+  let load = function() {
+    var d = document,
+      s = d.createElement("script");
+    s.src = "https://shidhincr.disqus.com/embed.js";
+    s.setAttribute("data-timestamp", +new Date());
+    (d.head || d.body).appendChild(s);
+  };
+
+  let elem = document.querySelector("[data-load-disqus]");
+  if (elem) {
+    elem.addEventListener(
+      "click",
+      function handle(params) {
+        elem.innerHTML = "Loading Comments ...";
+        load();
+        setTimeout(() => {
+          var disqusDiv = document.querySelector('#disqus_thread');
+          if(disqusDiv) {
+            disqusDiv.scrollIntoView();
+          }
+          elem.remove();
+        }, 3000);
+      },
+      { once: true }
+    );
+  }
+})();
